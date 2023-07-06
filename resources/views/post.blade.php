fcda2d3ef3b1e0e2b4287646f9a1bd4e50b52e05
JWL <jwl.vdonsk@gmail.com>
2023-07-06 16:41:53 +0300

    Laravel 8 From Scratch. Episode 8

---
+++ resources/views/post.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+
+<title>My Blog</title>
+<link rel="stylesheet" href="/app.css">
+
+<body>
+    <article>
+        <?= $post; ?>
+    </article>
+    <p>
+        <a href="/">Go back</a>
+    </p>
+</body>
---
+++ resources/views/posts.blade.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+
+<title>My Blog</title>
+<link rel="stylesheet" href="/app.css">
+
+<body>
+    <article>
+        <h1><a href="/post">My First Post</a></h1>
+
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent eget purus fermentum, efficitur massa sit amet, euismod quam. Donec ut porta felis. Etiam sit amet scelerisque ante. Phasellus a tellus at diam ornare porttitor. Ut pretium sagittis luctus. Praesent in ultricies magna, vitae lacinia lectus. Nam augue augue, placerat eget elementum ut, rhoncus sed nulla. Suspendisse cursus augue ac iaculis posuere. Sed interdum, nibh id fermentum imperdiet, ante mauris faucibus ante, hendrerit consequat orci felis quis metus. Cras mattis pulvinar tellus et mattis. Praesent sodales diam et lacus facilisis efficitur. Praesent ullamcorper luctus turpis elementum commodo. Sed eu libero lectus. Sed rhoncus tincidunt rutrum. Integer in consequat ipsum. Sed eget sem posuere, consequat nibh in, ullamcorper libero.
+        </p>
+    </article>
+
+    <article>
+        <h1><a href="/post">My Second Post</a></h1>
+
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent eget purus fermentum, efficitur massa sit amet, euismod quam. Donec ut porta felis. Etiam sit amet scelerisque ante. Phasellus a tellus at diam ornare porttitor. Ut pretium sagittis luctus. Praesent in ultricies magna, vitae lacinia lectus. Nam augue augue, placerat eget elementum ut, rhoncus sed nulla. Suspendisse cursus augue ac iaculis posuere. Sed interdum, nibh id fermentum imperdiet, ante mauris faucibus ante, hendrerit consequat orci felis quis metus. Cras mattis pulvinar tellus et mattis. Praesent sodales diam et lacus facilisis efficitur. Praesent ullamcorper luctus turpis elementum commodo. Sed eu libero lectus. Sed rhoncus tincidunt rutrum. Integer in consequat ipsum. Sed eget sem posuere, consequat nibh in, ullamcorper libero.
+        </p>
+    </article>
+
+    <article>
+        <h1><a href="/post">My Third Post</a></h1>
+
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent eget purus fermentum, efficitur massa sit amet, euismod quam. Donec ut porta felis. Etiam sit amet scelerisque ante. Phasellus a tellus at diam ornare porttitor. Ut pretium sagittis luctus. Praesent in ultricies magna, vitae lacinia lectus. Nam augue augue, placerat eget elementum ut, rhoncus sed nulla. Suspendisse cursus augue ac iaculis posuere. Sed interdum, nibh id fermentum imperdiet, ante mauris faucibus ante, hendrerit consequat orci felis quis metus. Cras mattis pulvinar tellus et mattis. Praesent sodales diam et lacus facilisis efficitur. Praesent ullamcorper luctus turpis elementum commodo. Sed eu libero lectus. Sed rhoncus tincidunt rutrum. Integer in consequat ipsum. Sed eget sem posuere, consequat nibh in, ullamcorper libero.
+        </p>
+    </article>
+</body>
