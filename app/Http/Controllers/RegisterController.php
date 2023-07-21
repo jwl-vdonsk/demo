bc51f68fc7637fdf4c55100cd807a5a58093e56b
JWL <jwl.vdonsk@gmail.com>
2023-07-21 16:00:29 +0300

    Laravel 8 From Scratch. Episode 53

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
 
 class RegisterController extends Controller
 {
@@ -14,14 +15,16 @@ class RegisterController extends Controller
     {
 
         $attributes = request()->validate([
-            'name' => ['required', 'max:255'],
-            'username' => ['required', 'max:255', 'min:3'],
-            'email' => ['required', 'email', 'max:255'],
-            'password' => ['required', 'max:255', 'min:1']
+            'name' => ['required', 'max:255', 'min:3'],
+            'username' => ['required', 'max:255', 'min:3', Rule::unique('users','username')],
+            'email' => ['required', 'email', 'max:255', Rule::unique('users','email')],
+            'password' => ['required', 'max:255', 'min:2']
         ]);
 
-        User::create($attributes);
+        $user = User::create($attributes);
 
-        return redirect('/');
+        auth()->login($user);
+
+        return redirect('/')->with('success', 'Your account has been created');
     }
 }
--- resources/views/sessions/create.blade.php
+++ resources/views/sessions/create.blade.php
@@ -1,28 +1,10 @@
 <x-layout>
     <section class="px-6 py-8">
         <main class="max-w-lg mx-auto mt-20 bg-gray-100 border border-black border-opacity-5 p-6 rounded-xl">
-            <h1 class="text-center font-bold text-xl">Register New User</h1>
+            <h1 class="text-center font-bold text-xl">Log In</h1>
 
-            <form method="POST" action="/register" class="mt-10">
+            <form method="POST" action="/sessions" class="mt-10">
                 @csrf
-                <div class="mb-6">
-                    <label class="block mb-2 uppercase font-bold text-xs text-gray-700"
-                           for="username">
-                        Username
-                    </label>
-
-                    <input class="border border-gray-400 p-2 w-full"
-                           type="text"
-                           name="username"
-                           value="{{ old('username') }}"
-                           id="username"
-                           required
-                    >
-
-                    @error('username')
-                        <p class="text-red-500 text-xs mt-2">{{ $message }}</p>
-                    @enderror
-                </div>
 
                 <div class="mb-6">
                     <label class="block mb-2 uppercase font-bold text-xs text-gray-700"
@@ -39,26 +21,7 @@
                     >
 
                     @error('email')
-                        <p class="text-red-500 text-xs mt-2">{{ $message }}</p>
-                    @enderror
-                </div>
-
-                <div class="mb-6">
-                    <label class="block mb-2 uppercase font-bold text-xs text-gray-700"
-                           for="name">
-                        name
-                    </label>
-
-                    <input class="border border-gray-400 p-2 w-full"
-                           type="text"
-                           name="name"
-                           id="name"
-                           value="{{ old('name') }}"
-                           required
-                    >
-
-                    @error('name')
-                        <p class="text-red-500 text-xs mt-2">{{ $message }}</p>
+                    <p class="text-red-500 text-xs mt-2">{{ $message }}</p>
                     @enderror
                 </div>
 
@@ -76,14 +39,14 @@
                     >
 
                     @error('password')
-                        <p class="text-red-500 text-xs mt-2">{{ $message }}</p>
+                    <p class="text-red-500 text-xs mt-2">{{ $message }}</p>
                     @enderror
                 </div>
 
                 <div class="mb-6">
                     <button type="submit"
                             class="bd-blue-400 text-black rounded py-2 px-4 hover:bg-blue-500 hover:text-white">
-                        Submit
+                        Log IN
                     </button>
                 </div>
 
